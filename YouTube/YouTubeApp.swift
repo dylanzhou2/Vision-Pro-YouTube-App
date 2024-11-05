2a1c9052f0f2fe56355a8db40cb5b689b1f663f8
Dylan Zhou <dylanzhou23@gmail.com>
2024-11-04 21:58:44 -0800

    Initial Commit

---
+++ YouTube/YouTubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouTubeApp.swift
+//  YouTube
+//
+//  Created by Dylan Zhou on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct YouTubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YouTubeTests/YouTubeTests.swift
@@ -0,0 +1,17 @@
+//
+//  YouTubeTests.swift
+//  YouTubeTests
+//
+//  Created by Dylan Zhou on 11/4/24.
+//
+
+import Testing
+@testable import YouTube
+
+struct YouTubeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
